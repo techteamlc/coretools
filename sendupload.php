63ee4724fc5e7f2b738362b23101839e9c762bad
Alexander Chu <webchu@gmail.com>
2020-07-22 18:23:39 -0300

    image uploader

--- sendupload.php
+++ sendupload.php
@@ -1,5 +1,13 @@
 <?php 
+header('Content-Type: application/json');
+
 if (isset($_POST['replace']) && !empty($_POST['replace'])) :
     echo 'replace';
 endif;
-print_r($_POST);
+
+//print_r($_POST);
+sleep(rand(1,5));
+echo json_encode(array(
+    'status' => true,
+    'name' => $_POST['name']
+));
