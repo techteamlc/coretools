3af0194ad454107232578a19d6cf19f7a25ffc75
Douglas Smolareki Vieira <douglas.vieira@ezcommerce.local>
2020-07-23 19:14:27 -0300

    testando post da imagem

--- imageuploader/sendupload.php
+++ imageuploader/sendupload.php
@@ -2,7 +2,22 @@
 header('Content-Type: application/json');
 
 if (isset($_POST['replace']) && !empty($_POST['replace'])) :
-    echo 'replace';
+    //echo 'replace';
+    $url = 'https://image.uploader:123@helpdesk.layer.core.dcg.com.br/v1/Hub/API.svc/web/SaveCatalogMedia'
+    $data = '{"ProductID": "'.$_POST['i.name'].'", "IntegrationID": null, },"Image": { "EncodedBase64File": {"FileName": "'.$_POST['i.name'].'", "ContentFileEncodedBase64": "'.$_POST['i.base64'].'", "ContentType": "'.$_POST['i.type'].'"} },"KeepOnlyMedia": true, "ReplaceExistingMedia": true}';
+    $options = array(
+	    'http' => array(
+	    	'header' => array( 
+	            'Accept: application/json', 
+	            'Content-Type: application/json'
+	        ),
+	        'method'  => 'POST',
+	        'content' => ($data)
+	    )
+	);
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+
 endif;
 
 //print_r($_POST);
