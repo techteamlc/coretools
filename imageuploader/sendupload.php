db3e2d06792c10a28f637f503ca37ca3017976c7
Alexander Chu <webchu@gmail.com>
2020-08-05 18:15:27 -0300

    criada fn pra salvar a media

--- imageuploader/sendupload.php
+++ imageuploader/sendupload.php
@@ -1,6 +1,48 @@
 <?php 
 header('Content-Type: application/json');
 
+function saveCatalogMedia($data) {
+	
+	global $name, $type, $separator, $replace, $keep, $idProduto;
+
+	$url = 'https://helpdesk.layer.core.dcg.com.br/v1/Catalog/API.svc/web/SaveCatalogMedia';
+    //$data = '{"ProductID": "103", "IntegrationID": null, "Image": { "EncodedBase64File": {"FileName": "'.$_POST['name'].'", "ContentFileEncodedBase64": "'.$img.'", "ContentType": "jpeg"} },"KeepOnlyMedia": true, "ReplaceExistingMedia": true}';
+	$options = array(
+	    'http' => array(
+	    	'header' => array( 
+	            'Authorization: Basic aW1hZ2UudXBsb2FkZXI6MTIz',
+	            'Accept: application/json', 
+	            'Content-Type: application/json'
+	        ),
+	        'method'  => 'POST',
+	        'content' => ($data)
+	    )
+	);
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+
+	//echo $options;   
+	$json = json_decode($result,JSON_PRETTY_PRINT);
+
+	if (!isset($json['IsValid']) || is_null($json['IsValid'])) :
+		$file = './log/log_' . date('Ymd') . '.txt';
+		file_put_contents($file, date('d/m/Y H:i:s') . ' - Erro no arquivo: ' . $name . PHP_EOL, FILE_APPEND);
+	endif;
+	
+
+	return array(
+		'Name' => $name,
+		'FileName' => $name . '.' . $type,
+		'Separator' => $separator,
+		'ImageType' => $type,
+		'ReplaceMedia' => $replace,
+		'KeepOnlyMedia' => $keep,
+		'ID' => $idProduto,
+		'APIresponseSuccess' => $json['IsValid'],
+		'Message' => $json['Errors']
+	);
+}
+
 $img = str_replace('data:image/jpeg;base64,','',$_POST['base64']);
 
 $replace = isset($_POST['replace']);
@@ -9,9 +51,12 @@ $separator = $_POST['separator'];
 $type1 = $_POST['type'];
 $type = str_replace('image/','',$type1);
 $name = str_replace($type,'',$_POST['name']);
-$idProduto = strstr($name, $separator, true);
+if ($separator == '') :
+	$idProduto = $name;
+else : 
+	$idProduto = strstr($name, $separator, true);
+endif;
 //$idProduto = 'P_' . $idProduto;
-
 
 $field = $_POST['field']; 
 switch ($field){
@@ -75,47 +120,19 @@ switch ($field){
 } 
 //echo $img;
 
- if (isset($_POST)) :
-   
-    $url = 'https://helpdesk.layer.core.dcg.com.br/v1/Catalog/API.svc/web/SaveCatalogMedia';
-    //$data = '{"ProductID": "103", "IntegrationID": null, "Image": { "EncodedBase64File": {"FileName": "'.$_POST['name'].'", "ContentFileEncodedBase64": "'.$img.'", "ContentType": "jpeg"} },"KeepOnlyMedia": true, "ReplaceExistingMedia": true}';
-	$options = array(
-	    'http' => array(
-	    	'header' => array( 
-	            'Authorization: Basic aW1hZ2UudXBsb2FkZXI6MTIz',
-	            'Accept: application/json', 
-	            'Content-Type: application/json'
-	        ),
-	        'method'  => 'POST',
-	        'content' => ($data)
-	    )
-	);
-	$context  = stream_context_create($options);
-	$result = file_get_contents($url, false, $context);
-
-	//echo $options;   
-	$json = json_decode($result,JSON_PRETTY_PRINT);
-
-	if (!isset($json['IsValid']) || is_null($json['IsValid'])) :
-		$file = './log/log_' . date('Ymd') . '.txt';
-		file_put_contents($file, date('d/m/Y H:i:s') . ' - Erro no arquivo: ' . $name . PHP_EOL, FILE_APPEND);
-	endif;
-
-
-	echo json_encode(array(
-		'Name' => $name,
-		'FileName' => $name . '.' . $type,
-		'Separator' => $separator,
-		'ImageType' => $type,
-		'ReplaceMedia' => $replace,
-		'KeepOnlyMedia' => $keep,
-		'ID' => $idProduto,
-		'APIresponseSuccess' => $json['IsValid'],
-		'Message' => $json['Errors']
-	));
-
-
-
+ if (isset($_POST) && $field != 'SkuID') :
+ 
+	$return = saveCatalogMedia($data);
+	echo json_encode($return);
+	
+else :
+	
+	/**
+	 * Chamar o GetSKU
+	 * no response, popular o $data e chamar a função saveCatalogMedia
+	 */
+	$return = saveCatalogMedia($data);
+	echo json_encode($return);
 
 endif;
 
